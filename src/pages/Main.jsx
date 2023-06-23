3474233d36de1e6d611fe2effa8b216f856fe229
AnSeonJeong <asj081@naver.com>
2023-06-23 16:47:03 +0900

    feat : 로그인 성공 후 메인 페이지 알림 추가 & strickMode 주석 처리

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
 
 function Main() {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const success = searchParams.get("success");
+
+  useEffect(() => {
+    if (success) {
+      alert("환영합니다!");
+      setSearchParams("");
+    }
+  }, []);
+
   return <div></div>;
 }
 
