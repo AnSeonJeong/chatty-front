3302724c07bd403407a8d583dc6248b1bc297715
AnSeonJeong <asj081@naver.com>
2023-06-28 21:43:11 +0900

    feat : 회원가입 - 이미지 업로드/미리보기 & 이메일 유효성 검사 & 비밀번호 확인 추가

--- src/components/Regi.tsx
+++ src/components/Regi.tsx
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import profile from "../assets/profile_none.png";
 import "../styles/regi.scss";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function Regi() {
   const [email, setEmail] = useState("");
@@ -9,49 +10,98 @@ function Regi() {
   const [confirmPwd, setConfirmPwd] = useState("");
   const [nickname, setNickname] = useState("");
   const [intro, setIntro] = useState("");
+  const [imgFile, setImgFile] = useState("");
+  const [saveImg, setSaveImg] = useState<File>();
+  const imgRef = useRef<HTMLInputElement>(null);
 
-  const onSubmit = () => {
-    let formdata = new FormData();
+  const history = useNavigate();
 
-    axios.post("regi", formdata);
+  // 이미지 업로드 & 미리보기
+  const ImgLoader = () => {
+    if (
+      imgRef.current &&
+      imgRef.current.files &&
+      imgRef.current.files.length > 0
+    ) {
+      const file = imgRef.current.files[0];
+      setSaveImg(file);
+      const reader = new FileReader();
+      reader.onload = () => {
+        if (typeof reader.result === "string") {
+          setImgFile(reader.result);
+        }
+      };
+      reader.readAsDataURL(file);
+    }
   };
+
+  // 이메일 유효성 검사
+  const CheckEmail = () => {
+    // 이메일 정규식
+    const reg_email =
+      /^([0-9a-zA-Z_\.-]+)@([0-9a-zA-Z_-]+)(\.[0-9a-zA-Z_-]+){1,2}$/;
+
+    let msg = "";
+    if (email.length > 1 && !reg_email.test(email)) {
+      msg = "❌ 올바른 이메일 형식이 아닙니다.";
+    }
+    return <div className="err_message">{msg}</div>;
+  };
+
+  // 비밀번호 확인
+  const CheckPwd = () => {
+    let msg = "";
+    if (pwd !== confirmPwd) msg = "❌ 비밀번호가 일치하지 않습니다.";
+    return <div className="err_message">{msg}</div>;
+  };
+
+  const handleRegi = () => {};
 
   return (
     <div className="regi-container">
       <h2>회원가입</h2>
       <div className="regi-form">
-        <form name="frm" onSubmit={onSubmit} encType="multipart/form">
+        <form name="frm" onSubmit={handleRegi} encType="multipart/form">
           {/* 프로필 사진 업로드 */}
           <div className="profile">
             <div>
-              <img src={profile} alt="profile" />
+              <img src={imgFile ? imgFile : profile} alt="profile" />
             </div>
             <label htmlFor="file">프로필 사진 업로드</label>
-            <input type="file" id="file" />
+            <input
+              type="file"
+              id="file"
+              accept="image/*"
+              ref={imgRef}
+              onChange={ImgLoader}
+              name="profile"
+            />
           </div>
           {/* 회원 정보 입력 */}
           <div className="info">
             <input
               type="email"
               value={email}
-              placeholder="이메일"
+              placeholder="*이메일"
               onChange={(e) => setEmail(e.target.value)}
             />
+            <CheckEmail />
             <input
               type="password"
               value={pwd}
-              placeholder="비밀번호"
+              placeholder="*비밀번호"
               onChange={(e) => setPwd(e.target.value)}
             />
             <input
               type="password"
               value={confirmPwd}
-              placeholder="비밀번호 확인"
+              placeholder="*비밀번호 확인"
               onChange={(e) => setConfirmPwd(e.target.value)}
             />
+            <CheckPwd />
             <input
               value={nickname}
-              placeholder="이름"
+              placeholder="*닉네임"
               onChange={(e) => setNickname(e.target.value)}
             />
             <input
@@ -60,7 +110,12 @@ function Regi() {
               onChange={(e) => setIntro(e.target.value)}
             />
           </div>
-          <input type="submit" className="confirmBtn" value="확인"></input>
+          <input
+            type="submit"
+            className="confirmBtn"
+            value="확인"
+            name="regi_info"
+          ></input>
         </form>
       </div>
     </div>
