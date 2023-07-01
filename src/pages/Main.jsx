f11720683b4fd1b71c0ba2cc26cc32fd8d72996e
AnSeonJeong <asj081@naver.com>
2023-07-02 02:03:06 +0900

    feat : side menu bar 추가 & 사용자 정보 요청과 관련된 처리 추가

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,19 +1,82 @@
-import React, { useEffect } from "react";
-import { useSearchParams } from "react-router-dom";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import side_logo from "../assets/side_logo.png";
+import "../styles/main.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCommentDots,
+  faUserCircle,
+  faUserFriends,
+} from "@fortawesome/free-solid-svg-icons";
+import profileNone from "../assets/profile_none.png";
 
 function Main() {
-  const [searchParams, setSearchParams] = useSearchParams();
-
-  const success = searchParams.get("success");
+  const history = useNavigate();
+  const [nickname, setNickname] = useState("");
+  const [profileUrl, setProfileUrl] = useState("");
+  const [email, setEmail] = useState("");
 
   useEffect(() => {
-    if (success) {
-      alert("환영합니다!");
-      setSearchParams("");
-    }
+    axios
+      .get("/main", { withCredentials: true })
+      .then((res) => {
+        console.log(res.data);
+        setProfileUrl(res.data.profileUrl);
+        setNickname(res.data.nickname);
+        setEmail(res.data.email);
+      })
+      .catch((err) => {
+        console.log(err);
+        alert("잘못된 접근입니다.");
+        history("/login");
+      });
   }, []);
 
-  return <div></div>;
+  return (
+    <div>
+      <div className="side_menu">
+        <div className="side_logo">
+          <img src={side_logo} />
+        </div>
+        <ul>
+          <Link to="chats">
+            <li>
+              <span className="icon">
+                <FontAwesomeIcon icon={faCommentDots} />
+              </span>
+              <span className="menu">Chats</span>
+            </li>
+          </Link>
+          <Link to="friends">
+            <li>
+              <span className="icon">
+                <FontAwesomeIcon icon={faUserFriends} />
+              </span>
+              <span className="menu">Friends</span>
+            </li>
+          </Link>
+          <Link to="profile">
+            <li>
+              <span className="icon">
+                <FontAwesomeIcon icon={faUserCircle} />
+              </span>
+              <span className="menu">Profile</span>
+            </li>
+          </Link>
+        </ul>
+        <div className="user">
+          <div className="user_profile">
+            <img src={profileUrl !== "" ? profileUrl : profileNone} />
+          </div>
+          <div className="user_con">
+            <span className="user_nickname">{nickname}</span>
+            <span className="user_email">{email}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Main;
