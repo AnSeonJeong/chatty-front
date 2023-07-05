83694ce61140a6addd895bf1dc80b063a3ca1f6c
AnSeonJeong <asj081@naver.com>
2023-07-05 16:01:08 +0900

    feat : ChatroomList 타입 선언 추가

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -7,3 +7,11 @@ type FriendList = {
   profileUrl: string;
   isFriend: boolean;
 };
+
+type ChatroomList = {
+  id: number; // 채팅방 번호
+  name: string; // 채팅방 이름
+  lastMessage: string; // 마지막 메시지
+  lastUpdatedAt: Date; // 마지막 메시지를 보낸 날짜
+  chatThumnail: string; // 채팅방 썸네일
+};
